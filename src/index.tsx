3815ca526bd80ee2326a6a3764ffcb21c1f27441
Kevin Wang <kwangsan@gmail.com>
2019-07-03 00:35:15 -0400

    export const withState

--- src/index.tsx
+++ src/index.tsx
@@ -2,22 +2,40 @@
  * @class ExampleComponent
  */
 
-import * as React from 'react'
+import * as React from "react";
 
-import styles from './styles.css'
+import styles from "./styles.css";
 
-export type Props = { text: string }
+export type Props = { text: string };
 
 export default class ExampleComponent extends React.Component<Props> {
   render() {
-    const {
-      text
-    } = this.props
+    const { text } = this.props;
 
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
+    return <div className={styles.test}>Example Component: {text}</div>;
   }
 }
+
+/**
+ * withState
+ * @param {string} stateName
+ * @param {string} stateUpdaterName
+ * @param {string} initialValue
+ */
+export const withState = (
+  stateName: string,
+  stateUpdaterName: string,
+  initialValue: string
+) => (Component: React.FC) => {
+  const Wrapper = React.memo(props => {
+    const [state, setState] = React.useState(initialValue);
+
+    const injectedProps = {
+      [stateName]: state,
+      [stateUpdaterName]: setState
+    };
+    return <Component {...props} {...injectedProps} />;
+  });
+
+  return Wrapper;
+};
